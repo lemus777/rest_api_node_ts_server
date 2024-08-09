f82af13d9472155d53b245f9ff1d4db0096e5f47
lemus777 <lemus777@gmail.com>
2024-08-09 19:46:00 +0200

    http metods

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,23 @@ const server = express()
 
 // Routing
 server.get('/', (req, res) => {
-
-  const datos = [
-    { id: 1, nombre: 'Juan' },
-    { id: 2, nombre: 'Pablo' },
-  ]
-
-  res.json(datos)
+  res.json('Desde GET')
 })
+
+server.post('/', (req, res) => {
+  res.json('Desde POST')
+})
+
+server.put('/', (req, res) => {
+  res.json('Desde PUT')
+})
+
+server.patch('/', (req, res) => {
+  res.json('Desde PATCH')
+})
+
+server.delete('/', (req, res) => {
+  res.json('Desde DELETE')
+})
+
 export default server
