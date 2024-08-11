1798eb16ec8025327bf21873aaeccf4a88e5a5db
lemus777 <lemus777@gmail.com>
2024-08-11 18:22:10 +0200

    adding products to DB

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,5 +1,9 @@
-export { Request, Response } from 'express'
+import { Request, Response } from 'express'
+import Product from '../models/Product.model'
 
-export const createProduct = (req : Request, res: Response) => {
-  res.json('Desde POST')
+export const createProduct = async (req : Request, res : Response) => {
+  
+  const product = await Product.create(req.body)
+
+  res.json({data: product})
 }
