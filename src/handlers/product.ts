1edb2dac186957b14889d83833d92e8dceac9220
lemus777 <lemus777@gmail.com>
2024-08-11 18:47:04 +0200

    validating new products in server

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from 'express'
+import { check, validationResult } from 'express-validator'
 import Product from '../models/Product.model'
 
 export const createProduct = async (req : Request, res : Response) => {
+  // Validación
+  await check('name')
+    .notEmpty().withMessage('El nombre del producto es obligatorio')
+    .run(req)
+  await check('price')
+    .isNumeric().withMessage('Valor no válido')
+    .notEmpty().withMessage('El precio del producto es obligatorio')
+    .custom(value => value > 0).withMessage('Precio no válido')
+    .run(req)
+
+  let errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
   
   const product = await Product.create(req.body)
 
