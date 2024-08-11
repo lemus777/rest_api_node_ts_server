4d46a59a632f9621ed633539f2bb7873da7fd922
lemus777 <lemus777@gmail.com>
2024-08-11 19:09:39 +0200

    moving the validation to the router

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,17 +1,8 @@
 import { Request, Response } from 'express'
-import { check, validationResult } from 'express-validator'
+import { validationResult } from 'express-validator'
 import Product from '../models/Product.model'
 
 export const createProduct = async (req : Request, res : Response) => {
-  // Validación
-  await check('name')
-    .notEmpty().withMessage('El nombre del producto es obligatorio')
-    .run(req)
-  await check('price')
-    .isNumeric().withMessage('Valor no válido')
-    .notEmpty().withMessage('El precio del producto es obligatorio')
-    .custom(value => value > 0).withMessage('Precio no válido')
-    .run(req)
 
   let errors = validationResult(req)
   if(!errors.isEmpty()) {
