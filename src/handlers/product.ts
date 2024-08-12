f789e2a03f49abfc6251fffe598e8d27b1154620
lemus777 <lemus777@gmail.com>
2024-08-12 13:45:32 +0200

    endpoint for deleteProduct created

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -73,3 +73,16 @@ export const updateAvailability = async (req : Request, res : Response) => {
 
   res.json({ data: product})
 }
+
+export const deleteProduct = async (req : Request, res : Response) => {
+  const { id } = req.params
+  const product = await Product.findByPk(id)
+
+  if(!product) {
+    return res.status(404).json({
+      error: 'Producto no encontrado'
+    })
+  }
+  await product.destroy()
+  res.json({data: 'Producto eliminado'})
+}
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { body, param } from 'express-validator'
-import { createProduct, getProductById, getProducts, updateAvailability, updateProduct } from './handlers/product'
+import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updateProduct } from './handlers/product'
 import { handleInputErrors } from './middleware'
 
 const router = Router()
@@ -27,7 +27,7 @@ router.post('/',
 )
 
 router.put('/:id',
-  // Validación
+  param('id').isInt().withMessage('ID no válido'),
   body('name')
     .notEmpty().withMessage('El nombre del producto es obligatorio'),
   body('price')
@@ -40,10 +40,16 @@ router.put('/:id',
   updateProduct
 )
 
-router.patch('/:id', updateAvailability)
+router.patch('/:id',
+  param('id').isInt().withMessage('ID no válido'),
+  handleInputErrors,
+  updateAvailability
+)
 
-router.delete('/', (req, res) => {
-  res.json('Desde DELETE')
-})
+router.delete('/:id',
+  param('id').isInt().withMessage('ID no válido'),
+  handleInputErrors,
+  deleteProduct
+)
 
 export default router
