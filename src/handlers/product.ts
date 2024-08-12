d911c79e0e973363a0af230fada1753c39eb7246
lemus777 <lemus777@gmail.com>
2024-08-12 13:33:44 +0200

    endpoint for updateAvailability created

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -56,3 +56,20 @@ export const updateProduct = async (req : Request, res : Response) => {
 
   res.json({ data: product})
 }
+
+export const updateAvailability = async (req : Request, res : Response) => {
+  const { id } = req.params
+  const product = await Product.findByPk(id)
+
+  if(!product) {
+    return res.status(404).json({
+      error: 'Producto no encontrado'
+    })
+  }
+
+  // Actualizar
+  product.availability = !product.dataValues.availability
+  await product.save()
+
+  res.json({ data: product})
+}
