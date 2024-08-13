dca1a38be5e1fdcc9ea7bb13dc70a145e965b125
lemus777 <lemus777@gmail.com>
2024-08-13 18:50:42 +0200

    testing delete products

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -164,3 +164,30 @@ describe('PUT /api/products/:id', () => {
     expect(response.body).not.toHaveProperty('errors')
   })
 })
+
+describe('DELETE /api/products/:id', () => {
+  it('Should check a valid ID', async () => {
+    const response = await request(server).delete('/api/products/not-valid')
+    expect(response.status).toBe(400)
+    expect(response.body).toHaveProperty('errors')
+    expect(response.body.errors[0].msg).toBe('ID no válido')
+  })
+
+  it('Should return a 404 response for a non-existent product', async () => {
+    const productId = 2000
+    const response = await request(server).delete(`/api/products/${productId}`)
+    expect(response.status).toBe(404)
+    expect(response.body.error).toBe('Producto no encontrado')
+
+    expect(response.status).not.toBe(200)
+  })
+
+  it('Should delete a product', async () => {
+    const response = await request(server).delete('/api/products/1')
+    expect(response.status).toBe(200)
+    expect(response.body.data).toBe('Producto eliminado')
+
+    expect(response.status).not.toBe(404)
+    expect(response.status).not.toBe(400)
+  })
+})
