c5d1bb7d30b4a848bc5736776209ad1cd28075d3
lemus777 <lemus777@gmail.com>
2024-08-13 12:05:49 +0200

    testing createProduct validation

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,6 +2,16 @@ import request from 'supertest'
 import server from '../../server'
 
 describe('POST /api/products', () => {
+  it('should display validation errors', async () => {
+    const response = await request(server).post('/api/products').send({})
+    expect(response.status).toBe(400)
+    expect(response.body).toHaveProperty('errors')
+    expect(response.body.errors).toHaveLength(4)
+
+    expect(response.status).not.toBe(404)
+    expect(response.body.errors).not.toHaveLength(2)
+  })
+
   it('Should create a new product', async () => {
     const response = await request(server).post('/api/products').send({
       name: "Mouse - testing",
@@ -12,6 +22,6 @@ describe('POST /api/products', () => {
 
     expect(response.status).not.toBe(200)
     expect(response.status).not.toBe(404)
-    expect(response.body).not.toHaveProperty('error')
+    expect(response.body).not.toHaveProperty('errors')
   })
 })
