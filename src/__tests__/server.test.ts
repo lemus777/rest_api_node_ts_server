80d48e686e038b9bfaa50fb6a188c6b677c95810
lemus777 <lemus777@gmail.com>
2024-08-14 11:30:38 +0200

    forcing db connection error with jest mock

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
-import server from "../server";
+import server, { connectDB} from "../server";
+import db from "../config/db";
 
 describe('GET /api', () => {
   it('Should send back a json response', async () => {
@@ -11,5 +12,20 @@ describe('GET /api', () => {
 
     expect(res.status).not.toBe(404)
     expect(res.body.msg).not.toBe('desde api')
+  })
+})
+
+jest.mock('../config/db')
+
+describe('connectDB', () => {
+  it('Should handle database connection error', async () => {
+    jest.spyOn(db, 'authenticate').mockRejectedValueOnce(new Error('Hubo un error al conectar a la DB'))
+    const consoleSpy = jest.spyOn(console, 'log')
+
+    await connectDB()
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      expect.stringContaining('Hubo un error al conectar a la DB')
+    )
   })
 })
