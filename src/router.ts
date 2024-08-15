7f1277fbb896fa1a41d3d3417f266de4e26ba104
lemus777 <lemus777@gmail.com>
2024-08-15 12:07:59 +0200

    docs - create product

--- src/router.ts
+++ src/router.ts
@@ -86,6 +86,34 @@ router.get('/:id',
   getProductById
 )
 
+/**
+ * @swagger
+ * /api/products:
+ *  post:
+ *    summary: Creates a new product
+ *    tags:
+ *      - Products
+ *    description: Returns a new record in the database
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                example: "Monitor curvo 49 pulgadas"
+ *              price:
+ *                type: number
+ *                example: 399
+ *    responses:
+ *      201:
+ *        description: Product created successfully
+ *      400:
+ *        description: Bad request - invalid input data
+ */
+
 router.post('/',
   // Validación
   body('name')
